b434a66dcbc3ad52625faf5c39044e7b7787e96f
Ilya91 <webmaster.1991@mail.ru>
2016-11-21 11:08:19 +0300

    new routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,46 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', 'TestController@show');
+Route::get('/admin', 'Admin\MainController@show');
+
+/*Route::post('/comments',function () {
+
+	print_r($_POST);
+
+});*/
+
+
+/*Route::match(['get','post'],'/comments',function () {
+
+	print_r($_POST);
+
+});*/
+
+/*Route::any('/comments',function () {
+
+	print_r($_POST);
+
+});*/
+/*
+Route::get('/page/{id}',function ($id) {
+
+    echo $id;
+
+});*/
+
+/*Route::get('/page/{id}/{cat}',function($var1) {
+
+    echo '<pre>';
+
+    echo $var1;
+    //print_r($_ENV);
+    //echo config('app.locale');
+    //echo Config::set('app.locale','ru');
+    //echo Config::get('app.locale');
+    ///echo env('APP_ENV');
+
+    echo '</pre>';
+
+})->where(['id'=>'[0-9]+','cat'=>'[A-Za-z]+']);*/
