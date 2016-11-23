2b0bca867ce5bcea2cfb2ea4cb0799079aa443ff
Ilya91 <webmaster.1991@mail.ru>
2016-11-23 14:43:15 +0300

    $request->url()

--- app/Http/Controllers/Admin/ContactsController.php
+++ app/Http/Controllers/Admin/ContactsController.php
@@ -13,9 +13,21 @@ class ContactsController
         $this->request = $request;
     }*/
 
-    public function show(Request $request){
+    public function show(Request $request, $id=FALSE){
         $array = array('title'=>'Laravel Project::Contacts');
+
+        echo '<h1 style="margin-top: 100px">'. $request->input('name', 'Default').'</h1>';
+
+        if ($request->has('site')){
             print_r($request->all());
-            return view('default.contacts', $array);
+        }
+        print_r($request->only('site', 'email'));
+        echo '<h1 style="margin-top:100px">'.$request->text.'</h1>';
+        print_r($request->path());
+
+        if ($request->is('contacts/*')){
+            echo '<h1 style="margin-top:100px">'.$request->url().'</h1>';
+        }
+        return view('default.contacts', $array);
     }
 }
