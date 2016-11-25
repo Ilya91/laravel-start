fb8d876ce4dc25b2b95657b11419bbf6d310c3ac
Ilya91 <webmaster.1991@mail.ru>
2016-11-25 18:34:20 +0300

    public function run()

--- database/seeds/NewsSeeder.php
+++ database/seeds/NewsSeeder.php
@@ -1,6 +1,6 @@
 <?php
-
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class NewsSeeder extends Seeder
 {
@@ -11,6 +11,28 @@ class NewsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::insert('INSERT INTO `news` (`name`, `text`, `img`) VALUES (?,?,?)', [
+        	'Blog post',
+	        '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+				Et exercitationem labore magnam modi molestiae optio quasi 
+				quisquam quo sapiente veritatis. Adipisci asperiores aspernatur, 
+				dolor eveniet officia provident quibusdam quisquam rem.</p>',
+	        'img1.jpg'
+        ]);
+
+	    DB::table('news')->insert(
+	    	[
+	    		[
+	    			'name' => 'Blog post',
+				    'text' => 'lorem lorem',
+				    'img' => 'img2.jpg'
+			    ],
+			    [
+				    'name' => 'Title2',
+				    'text' => 'Text2',
+				    'img' => 'img3.jpg'
+			    ]
+		    ]
+	    );
     }
 }
