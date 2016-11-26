857b9e53086fb4b485e5f9c4d02b716b17937337
Ilya91 <webmaster.1991@mail.ru>
2016-11-26 16:15:33 +0300

    protected $guarded

--- app/News.php
+++ app/News.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    //
+    protected $table = 'news';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    public $timestamps = true;
+    protected $fillable = ['name', 'text'];
+    protected $guarded = ['*'];
+
 }
