2dd06ad7de703c65848641ebcbf2c16430b499c7
Ilya91 <webmaster.1991@mail.ru>
2016-11-26 11:22:50 +0300

    '<img src='.$new->img.'>'

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -4,10 +4,36 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use DB;
 
 class MainController extends Controller
 {
-    public function show(){
-        echo __METHOD__;
+    protected static $news;
+    /*public function __construct(){
+        $this->middleware('mymiddle');
+    }*/
+
+    public static function addNews($array){
+        return self::$news[] = $array;
+    }
+
+    public function getArticles(){
+        $news = DB::table('news')->get();
+        foreach ($news as $new) {
+            echo '<img src='.$new->img.'>';
+        }
+        //$news = DB::table('news')->first();
+        //$news = DB::table('news')->where('id', 4)->value('name');
+        /*DB::table('news')->chunk(2, function($news) {
+            foreach ($news as $new) {
+                self::addNews($new);
+            }
+        });
+        dump(self::$news);*/
+        //dump($news);
+    }
+
+    public function getArticle($id){
+        echo $id;
     }
 }
