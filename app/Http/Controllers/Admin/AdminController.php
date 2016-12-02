2f3857b73ba5c93dfc36df633b7b008fedcf1664
Ilya91 <webmaster.1991@mail.ru>
2016-12-02 18:18:24 +0300

    public function show()

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,10 +2,37 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use \Auth;
+
 
 class AdminController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function show(){
+        $user = Auth::user();
+
+        if(!Auth::check()){
+            //$user = User::find(10);
+
+            //Auth::guard('web')->login($user);
+            Auth::guard('web')->logout();
+            Auth::loginUsingId(9);
+            //dump($user);
+            //return redirect('login');
+        };
+
+        if(Auth::viaRemember()){
+            echo 'yes';
+        };
+
+        //dump(Auth::id());
+        return view('home');
+    }
 }
