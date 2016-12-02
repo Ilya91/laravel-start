286cfcfb1baa444e5fe2d2aa00b380547904e230
Ilya91 <webmaster.1991@mail.ru>
2016-12-02 18:59:23 +0300

    AdminUpdatePostController

---
+++ app/Http/Controllers/Admin/AdminUpdatePostController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\News;
+use Auth;
+
+use Gate;
+
+class AdminUpdatePostController extends Controller
+{
+    //show Form
+    public function show(Request $request, $id) {
+
+    	$news = News::find($id);
+		return view('default.update_post',['title' => 'Редактирование материала','news' => $news]);
+	}
+	
+	//new post
+    public function create(Request $request) {
+    	
+    	$this->validate($request,[
+    		'name'=>'required'
+    	]);
+    	
+    	$user = Auth::user();
+    	
+    	$data = $request->except('_token');
+    	
+    	$article = News::find($data['id']);
+    	
+
+			$article->name = $data['name'];
+	    	$article->img = $data['img'];
+	    	$article->text = $data['text'];
+	    	
+	    	$res = $user->news()->save($article);
+	        
+	       
+			return redirect()->back()->with('message','Материал обновлен');
+
+    	
+
+    	
+       
+	}
+}
